a5e51f4b26ec5efa88dbaf4508c1d9a9278fa726
Hieu Ngo <enginoobz@gmail.com>
2021-02-02 15:12:37 +0100

    Use TestMesh to display coordinate on the grid

--- Assets/Scripts/SnapEditor.cs
+++ Assets/Scripts/SnapEditor.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class SnapEditor : MonoBehaviour
 {
-    [SerializeField] [Range(1f, 20f)] int gridSize=10;
+    [SerializeField] [Range(1f, 20f)] int gridSize = 10;
+    [SerializeField] TextMesh coordLabel;
 
     Vector3 snapPos;
     // Start is called before the first frame update
@@ -20,5 +22,7 @@ public class SnapEditor : MonoBehaviour
         snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
         snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
         transform.position = snapPos;
+
+        coordLabel.text = snapPos.x / gridSize + "," + snapPos.z / gridSize;
     }
 }
