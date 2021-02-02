099eedafd8d1829509d30bbf3698f00a7ecfae1c
Hieu Ngo <enginoobz@gmail.com>
2021-02-02 14:46:46 +0100

    Implement snap feature in edit mode

---
+++ Assets/Scripts/SnapEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SnapEditor : MonoBehaviour
+{
+    [SerializeField] [Range(1f, 20f)] int gridSize=10;
+
+    Vector3 snapPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        snapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+        transform.position = snapPos;
+    }
+}
