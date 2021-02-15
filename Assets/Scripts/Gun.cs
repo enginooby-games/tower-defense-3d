2737dcd6ab81f5cd4756149801fa5877ef9212e3
Hieu Ngo <enginoobz@gmail.com>
2021-02-15 18:12:08 +0100

    Implement projectile shooting for towers

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject firePoint;
+    public GameObject bulletVfx;
+    public float fireRate;
+
+    private int count = 0;
+    private float timeToFire = 0f;
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if (Time.time >= timeToFire)
+        {
+            timeToFire = Time.time + 1f / fireRate;
+            SpawnVFX();
+        }
+    }
+
+    public void SpawnVFX()
+    {
+        GameObject bullet;
+
+        if (firePoint != null)
+        {
+            bullet = Instantiate(bulletVfx, firePoint.transform.position, Quaternion.identity);
+            bullet.transform.localRotation = firePoint.transform.rotation;
+        }
+        else
+            bullet = Instantiate(bulletVfx);
+
+        bullet.transform.parent = gameObject.transform;
+        // var ps = bullet.GetComponent<ParticleSystem>();
+
+        // if (bullet.transform.childCount > 0)
+        // {
+        //     ps = bullet.transform.GetChild(0).GetComponent<ParticleSystem>();
+        // }
+    }
+}
