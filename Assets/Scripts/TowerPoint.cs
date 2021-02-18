26f2b213a6211b90364c3925769de71aa10cea5a
Hieu Ngo <enginoobz@gmail.com>
2021-02-18 07:50:28 +0100

    Implement tower block to build tower

---
+++ Assets/Scripts/TowerPoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPoint : MonoBehaviour
+{
+    [SerializeField] GameObject towerPrefab;
+
+    bool isOccupied = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnMouseDown()
+    {
+        if (!isOccupied)
+        {
+            GameObject tower = Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isOccupied = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
