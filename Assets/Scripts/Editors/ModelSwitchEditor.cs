426da4ea81248b9e412b26e08c77f86dd463686b
Hieu Ngo <enginoobz@gmail.com>
2021-02-21 13:28:17 +0100

    [Editor] Implement model switch

---
+++ Assets/Scripts/Editors/ModelSwitchEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+[ExecuteInEditMode]
+public class ModelSwitchEditor : MonoBehaviour
+{
+    [SerializeField] List<GameObject> models = new List<GameObject>();
+    [Range(0, 2)] public int activeModelIndex = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Undo.RecordObject(gameObject, "descriptive name of this operation");
+        // Undo.RecordObject(gameObject.GetComponent<ModelSwitchEditor>(), "Switch model");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnValidate()
+    {
+
+        for (int i = 0; i < models.Count; i++)
+        {
+            models[i].SetActive(i == activeModelIndex);
+        }
+
+        // TODO: Apply prefab when make changes on instances
+        // UnityEditor.PrefabUtility.RecordPrefabInstancePropertyModifications(this);
+        // PrefabUtility.ApplyPrefabInstance(gameObject, InteractionMode.UserAction); // freeze editor!
+    }
+}
---
+++ Assets/Scripts/Editors/ModelSwitchManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+
+[ExecuteInEditMode]
+public class ModelSwitchManager : MonoBehaviour
+{
+    [Range(0, 2)] [SerializeField] int activeModelIndex = 0;
+    [SerializeField] List<ModelSwitchEditor> prefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnValidate()
+    {
+        foreach (ModelSwitchEditor prefab in prefabs)
+        {
+            prefab.activeModelIndex = activeModelIndex;
+        }
+
+        // TODO: auto save scene after make change on this manager
+        // EditorSceneManager.SaveScene(SceneManager.GetActiveScene());
+    }
+}
